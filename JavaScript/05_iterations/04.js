d3b6df209337f7be4cde4249f110d2acf8a1d1ea
DhruvPekhale <dnpekhale@gmail.com>
2025-06-21 08:50:53 +0530

    initial

--- JavaScript/05_iterations/04.js
+++ JavaScript/05_iterations/04.js
@@ -16,7 +16,4 @@ for (const key in programming) {
     console.log(programming[key]);
 }
 
-const map = new Map()
-map.set('IN',"India")
-map.set("USA","Unites States of America")
-map.set("Fr","France")
+
